8622d16bf62f5150d5f03d0e6e61db57a842b9b1
jamestang12 <homing.tang@ryerson.ca>
2021-11-14 19:38:47 -0500

    update

--- controllers/items.js
+++ controllers/items.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async')
+const Item = require('../models/Item')
 
 //@desc    Get all items
 //@route   GET /api/v1/items
@@ -7,3 +8,26 @@ const asyncHandler = require('../middleware/async')
 exports.getItems = asyncHandler(async (req, res,next) => {
     res.status(200).json({success: true, data: "Hello World"})
 })
+
+//@desc    Create items
+//@route   Post /api/v1/items
+//@access  Private
+exports.createItem = asyncHandler(async (req, res,next) => {
+    
+    console.log(req.body)
+    // const result = await Item.create(req.body);
+    // console.log(result)
+    res.status(200).json({success: true, data: "result"})
+})
+
+//@desc    Create items
+//@route   Post /api/v1/items/image
+//@access  Private
+exports.uploadImageForCreateItem = asyncHandler(async (req, res,next) => {
+    
+    console.log(req.body)
+    // const result = await Item.create(req.body);
+    // console.log(result)
+    res.status(200).json({success: true, data: "result"})
+})
+
--- routes/items.js
+++ routes/items.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router();
 
-const { getItems } = require('../controllers/items')
+const { getItems, createItem, uploadImageForCreateItem } = require('../controllers/items')
 
 router.get('/', getItems)
+router.post('/',createItem)
+router.post('/image', uploadImageForCreateItem)
 
 module.exports = router;
