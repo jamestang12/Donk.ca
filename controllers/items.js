d618157d181b532480ab2ca90b78b1937a0098d0
Joshua Huerto <joshuahuerto2@gmail.com>
2021-11-14 19:36:27 -0500

    Revert "Setting up items route"

--- controllers/items.js
+++
@@ -1,9 +0,0 @@
-const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('../middleware/async')
-
-//@desc    Get all items
-//@route   GET /api/v1/items
-//@access  Public
-exports.getItems = asyncHandler(async (req, res,next) => {
-    res.status(200).json({success: true, data: "Hello World"})
-})
--- routes/items.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const { getItems } = require('../controllers/items')
-
-router.get('/', getItems)
-
-module.exports = router;
